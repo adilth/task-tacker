338d03bb3e1519cab663913cf35088a57fca46f4
adil tahiri <rajaadil1995@gmail.com>
2022-10-16 21:00:20 +0100

    add about page

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h4>Know More About Me </h4>
+      <a href="https://adilth.netlify.app/">My Portfolio</a>
+      <a href="https://www.linkedin.com/in/adil-tahiri/">My linkedin</a>
+      <a href="https://github.com/adilth">My github</a>
+      <a href="/">Go back </a>
+    </div>
+  );
+};
+
+export default About;
